2f1a26d2fc21078ce5bad939d109a5a0480a5a5a
Kevin Vognar <kvognar@gmail.com>
2026-07-22 16:28:13 -0700

    click on the screen to do nothing!

--- workstation.gd
+++ workstation.gd
@@ -1,3 +1,11 @@
 extends Node3D
 
 var games: Array[Microgame] = []
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event.is_action_pressed("action"):
+		print_debug('monitor clicked!')
