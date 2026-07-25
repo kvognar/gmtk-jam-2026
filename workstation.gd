0adc489781d0c6fd1ab63b657257229feef79bb6
Kevin Vognar <kvognar@gmail.com>
2026-07-24 21:07:51 -0700

    counter ticks down with the games

--- workstation.gd
+++ workstation.gd
@@ -9,11 +9,18 @@ var losses := 0
 
 var game_index = 0
 
+var game_count_set := false
+var total_games := 0
+
 func _ready() -> void:
 	MusicPlayer.switch_to(song)
 	#countdown.play()
 	show_scores()
 	for container: MicrogameContainer in get_tree().get_nodes_in_group('microgame_containers'):
+		if !game_count_set:
+			total_games = container.games.size()
+			%GamesLeftCounter.set_value(total_games)
+			game_count_set = true
 		container.failure.connect(_on_game_fail)
 		container.success.connect(_on_game_success)
 		container.complete.connect(_on_games_completed)
@@ -33,9 +40,11 @@ func _on_game_success() -> void:
 	show_scores()
 
 func _on_games_completed() -> void:
+	await get_tree().create_timer(1.5).timeout
 	%EndingOrchestrator.show_ending(wins, losses)
 	print_debug('its all done')
 
 func show_scores() -> void:
 	%Wins.text = 'Wins: ' + str(wins)
 	%Losses.text = 'Losses: ' + str(losses)
+	%GamesLeftCounter.set_value(total_games - wins - losses)
