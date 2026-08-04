60018405320293c9f7bfadc14d1516e275a13d26
Kevin Vognar <kvognar@gmail.com>
2026-08-03 22:11:04 -0700

    fixed pigeon animations, sounds, hitboxes

--- entities/pigeon.gd
+++ entities/pigeon.gd
@@ -11,33 +11,38 @@ var waiting_animations = ['stand', 'walk']
 
 @export var manual = true
 
+@export_enum('stand', 'walk', 'nesting', 'nest') var starting_pose: String = 'stand'
+
 signal shooed
 
 func _ready() -> void:
-	if !manual:
-		$AnimatedSprite2D.play(waiting_animations.pick_random())
+	if starting_pose:
+		$AnimationPlayer.play(starting_pose)
+	#if !manual:
+		#$AnimationPlayer.play(waiting_animations.pick_random())
 
 func shoo(broom: Node2D) -> void:
 	if state == STATES.waiting:
 		state = STATES.fleeing
 		shooed.emit()
-		if $AnimatedSprite2D.animation == 'nest':
+		print_debug($AnimationPlayer.current_animation)
+		if $AnimationPlayer.current_animation == 'nest':
 			fall()
 		else:
 			fly_away(broom)
 
 func fall() -> void:
-	$AnimatedSprite2D.flip_h = true
+	$Sprite2D.flip_h = true
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, 'global_position', global_position + Vector2(0, 800), 1)
 
 
 func fly_away(broom: Node2D) -> void:
-	$AnimatedSprite2D.play("fly")
+	$AnimationPlayer.play("fly")
 	var flee_vector = (global_position - broom.global_position).normalized()
 	flee_point = global_position + flee_vector * 1000
-	$AnimatedSprite2D.flip_h = flee_point.x < global_position.x
-	$AnimationPlayer.play("flap")
+	$Sprite2D.flip_h = flee_point.x < global_position.x
+	$EffectsAnimationPlayer.play("flap")
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, 'global_position', flee_point, 1.0)
 	$AudioStreamPlayer2D.play()
